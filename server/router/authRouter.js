68abff9668c9b7f638d1e1fe1cf19f58ed35e027
Daniel <daniel353a3@gmail.com>
2024-02-19 08:55:07 +0200

    Added registration and login functionality

---
+++ server/router/authRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const userBLL = require('../BLL/userBLL');
+
+router.post('/registration', async (req, res) => {
+    try {
+        const userData = req.body;
+        const response = await userBLL.registration(userData)
+
+        res.status(201).send(response)
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const response = await userBLL.login(email, password);
+
+        console.log(response);
+        res.send(response)
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+module.exports = router;
