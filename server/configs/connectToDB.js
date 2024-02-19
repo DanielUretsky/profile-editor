cf8a2853b31c13d67fec9a92e9ffc965d2abf8d3
Daniel <daniel353a3@gmail.com>
2024-02-19 08:58:59 +0200

    some changes

--- server/configs/connectToDB.js
+++ server/configs/connectToDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToDB = () => {
-    mongoose.connect('mongodb://localhost:27017/profile-editor')
+    mongoose.connect('mongodb://127.0.0.1:27017/profile-editor')
             .then(() => console.log('Database connected succedfully'))
             .catch(err => console.log(err));
 }
