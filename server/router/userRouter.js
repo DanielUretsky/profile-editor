ff92984fcb901bc54fcab74180823f9f017ab9ad
Daniel <daniel353a3@gmail.com>
2024-02-20 02:41:05 +0200

    added test route for testing authentication

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const tokenAuthentication = require('../configs/tokenAuthentication');
+
+router.get('/test',tokenAuthentication, async(req, res) => {
+    res.send("succes")
+})
 
 module.exports = router;
