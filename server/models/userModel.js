18452ea3a44f5e4f029404c2788b663318f4e2e7
Daniel <daniel353a3@gmail.com>
2024-02-20 02:43:19 +0200

    changed userModel, added more properties

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    "username": {type: String, required: true},
-    "email": {type: String, required: true},
-    "password": {type: String, required: true},
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    gender: {type: String, required: true},
+    registrationDate: {type: Date, default: Date.now},
+    coinsBalance: {type: Number, default: 100}
 }, {
     versionKey: false,
     strict: true
