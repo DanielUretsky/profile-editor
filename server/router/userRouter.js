85f5a246519f3752af9ccc4abe437d2182cf9c40
Daniel <daniel353a3@gmail.com>
2024-02-21 03:40:33 +0200

    added new route for searching specific user

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 const tokenAuthentication = require('../configs/tokenAuthentication');
 
+const usersBll = require('../BLL/usersBLL');
+
 router.get('/test',tokenAuthentication, async(req, res) => {
     res.send("succes")
+});
+
+router.get('/purchases', async(req, res) => {
+    
+})
+
+router.get('/:id', async(req, res) => {
+    const id = req.params.id;
+    const response = await usersBll.getOneUser(id);
+    
+    res.send(response);
+
 })
 
 module.exports = router;
