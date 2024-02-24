fbe6ec48f51f379d7511e3a2cdd7e092243093d7
Daniel <daniel353a3@gmail.com>
2024-02-24 04:21:58 +0200

    created logic for buying and selling

--- server/BLL/productsBLL.js
+++ server/BLL/productsBLL.js
@@ -1,4 +1,5 @@
 const jsonfile = require('jsonfile');
+const userModel = require('../models/userModel');
 const purchaseModel = require('../models/purchaseModel');
 
 const getAllProducts = async() => {
@@ -16,17 +17,39 @@ const getProductByType = async (productType) => {
 const buyProduct = async(product) => {
     try {
         const newPurchase = new purchaseModel(product);
+        const user = await userModel.findOne({_id : newPurchase.user});
+
         if(!newPurchase) return "Something went wrong";
+        user.coinsBalance -= product.price;
+        
+        await user.save();
         await newPurchase.save();
 
-        return "Pruduct have been added";
+        return {userBalance: user.coinsBalance}
     } catch (err) {
         console.log(err);
     }
 }
 
+const sellProduct = async(product) => {
+    try {
+        const user = await userModel.findOne({_id : product.user});
+        await purchaseModel.findByIdAndDelete(product.id);
+        console.log(product);
+        console.log(typeof product.price);
+        user.coinsBalance += +product.price;
+        
+        await user.save();
+      
+        return {userBalance: user.coinsBalance}
+    } catch (err) {
+        throw err;
+    }
+}
+
 module.exports = {
-    getAllProducts,
+    getAllProducts,  
     getProductByType,
-    buyProduct
+    buyProduct,
+    sellProduct
 }
--- server/BLL/usersBLL.js
+++ server/BLL/usersBLL.js
@@ -1,54 +1,29 @@
 require('dotenv').config();
+
 const userModel = require('../models/userModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-
-const registration = async (userData) => {
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(userData.password, salt);
-        console.log(userData);
-        const newUser = new userModel({ ...userData, password: hashedPassword });
-        await newUser.save();
-
-        return "Created succedfully";
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-const login = async (email, password) => {
-    try {
-        const user = await userModel.findOne({ email });
-        const ifPasswordMatches = await bcrypt.compare(password, user.password) 
-        
-        if(!user || !ifPasswordMatches) return 'Unknown email or password!';
-        const tokenPayload = { email : user.email }
-        const accessToken = jwt.sign(tokenPayload, process.env.ACCESS_TOKEN_SECRET);
-
-        return {accessToken};
-
-    } catch (err) {
-        console.log(err);
-    }
-}
-
+const purchaseModel = require('../models/purchaseModel');
 
 const getOneUser = async (id) => {
     try {
         const user = await userModel.findOne({ _id: id });
-        console.log(user);
-       
-
         return user;
     } catch (err) {
-        throw err.message;
+        throw err;
+    }
+}
+
+const getUserPurchases = async (id) => {
+    try {
+        const purchases = await purchaseModel.find({user: id});
+        return purchases;
+    } catch (err) {
+        console.err(err);
+        throw err;
     }
 }
 
 module.exports = {
-    registration,
-    login,
-    getOneUser
-}
+    
+    getOneUser,
+    getUserPurchases
+} 
